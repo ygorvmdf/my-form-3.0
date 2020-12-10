84b05fb13bd728118a8cbb1f738adc5c9f8e780a
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-12-10 19:13:34 -0300

    primeiro fieldset

--- src/Forms.js
+++ src/Forms.js
@@ -5,6 +5,7 @@ class Forms extends React.Component {
     super(props);
 
     this.toUpperCase = this.toUpperCase.bind(this);
+    this.checkFirstLetter = this.checkFirstLetter.bind(this);
 
     this.state = {
       validated: true,
@@ -13,6 +14,13 @@ class Forms extends React.Component {
 
   toUpperCase({target}) {
     target.value = target.value.toUpperCase();
+  }
+
+  checkFirstLetter({ target }) {
+    const firstLetter = target.value[0];
+    if (Number(firstLetter) || firstLetter === '0') {
+      target.value = '';
+    }
   }
 
   render() {
@@ -26,6 +34,38 @@ class Forms extends React.Component {
             onChange={this.toUpperCase}
             required
           />
+          <label htmlFor="input-email">Email</label>
+          <input
+            id="input-email"
+            maxlength="50"
+            required
+          />
+          <label htmlFor="input-cpf">CPF</label>
+          <input
+            id="input-cpf"
+            maxlength="11"
+            required
+          />
+          <label htmlFor="input-adress">Endereço</label>
+          <input
+            id="input-adress"
+            maxlength="200"
+            required
+          />
+          <label htmlFor="input-city">Cidade</label>
+          <input
+            id="input-city"
+            maxlength="28"
+            onBlur={this.checkFirstLetter}
+            required
+          />
+          <select>
+
+          </select>
+          <label>Casa</label>
+          <input type="radio" name="type" />
+          <label>Apartamento</label>
+          <input type="radio" name="type" checked/>
         </fieldset>
       </section>
     );
